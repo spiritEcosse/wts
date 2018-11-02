bd71922c9e3bfd3071766f6cd795e1dd30595e9f
igor <shevchenkcoigor@gmail.com>
2018-11-02 17:40:54 +0200

    add html creator

---
+++ tests/test_web.py
@@ -0,0 +1,9 @@
+class TestClass(object):
+
+    def test_find_button(self):
+        button = ""
+        assert 'h' in x
+
+    def test_two(self):
+        x = "check"
+        assert hasattr(x, 'split')
