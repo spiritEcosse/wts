321517870e532fe41671ff0e04cfda1094166f7e
igor <shevchenkcoigor@gmail.com>
2018-12-07 16:27:15 +0200

    add command for forever up container web

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ Flask app.
 
 Import all models.
 """
-# from apps.test.views import case_page
 from wts.settings import PROJECT
 from wts.utils import create_app, create_db, create_migrate, import_models
 
@@ -12,5 +11,3 @@ db = create_db(app)
 migrate = create_migrate(app, db)
 
 import_models(apps_folder='apps')
-
-# app.register_blueprint(case_page)
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -6,6 +6,15 @@
 #
 # @pytest.mark.db
 # def test_builder(case):
+#     from wts.utils import create_driver
+#     import uuid
+#     driver = create_driver()
+#
+#     path = '/wts/tests/tmp/{}.html'.format(str(uuid.uuid4()))
+#     with open(path, "w") as f:
+#         f.write('<div class="temp"></div>')
+#     driver.get('file://{}'.format(path))
+
 #     url = 'http://web/{}/'.format(case.id)
 #     builder = Builder(bs_input=url)
 #     builder.run()
